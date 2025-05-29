4d1edcd8c1541730f378df76a8ec0d9341e90982
Sarmad  Rafiq <sarmadrafiq999@gmail.com>
2025-05-29 15:00:51 +0500

    Fix: updated MongoDB URI environment variable usage

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const mongoose = require("mongoose")
-const URI = process.env.DbPassword
+const URI = process.env.DB_PASSWORD
 const conectDb = async () => {
     try {
         await mongoose.connect(URI)
